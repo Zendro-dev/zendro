f7b71429623b5c67d42eacc018cb3c0c770e5af8
dan <wunderbardan@gmail.com>
2020-09-30 17:03:52 +0200

    feat: add new commands, e.g. dockerize and start

--- bin/index.js
+++ bin/index.js
@@ -5,17 +5,14 @@ const collect = (value, previous) => {return previous.concat([value])}
 program.version(require('../package').version)
 
 program
-    .command('init')
-    .description('init Zendro')
-    .action(require('../lib/init'))
-
-program
     .command('new <name>')
     .description('create new Zendro project')
+    .option('-d, --dockerize', 'include docker config files', false)
+    .option('-u, --update_code_generators', 'update code generators', false)
     .action(require('../lib/new'))
 
 program
-    .command('generate-gqs <name>')
+    .command('generate-gqs')
     .description('generate graphql-server code')
     .requiredOption('-d, --data_model_definitions <def_path>', 
     'path to a input directory or a JSON file', collect, [])
@@ -24,12 +21,23 @@ program
     .action(require('../lib/generate_gqs'))
 
 program
-    .command('generate-spa <name>')
+    .command('generate-spa')
     .description('generate single page application code')
     .requiredOption('-d, --data_model_definitions <def_path>', 
     'path to a input directory or a JSON file', collect, [])
     .option('-o, --output_dir <output_dir>', 'output directory')
     .option('-D, --createBaseDirs', 'create base directories', false)
     .action(require('../lib/generate_spa'))
+
+program
+    .command('dockerize')
+    .description('dockerize Zendro App')
+    .action(require('../lib/dockerize'))
+
+program
+    .command('start [service...]')
+    .description('start Zendro App')
+    .option('-i, --install_package', 'install packages', false)
+    .action(require('../lib/start'))
 
 program.parse(process.argv)
--- helper.js
+++ helper.js
@@ -1,11 +1,21 @@
 const { promisify } = require('util')
-const download = promisify(require('download-git-repo'));
-const ora = require('ora');
-const { spawn } = require('child_process');
+const download = promisify(require('download-git-repo'))
+const ora = require('ora')
+const { spawn } = require('child_process')
 const chalk = require('chalk')
+const fs = require('fs')
 
+/**
+ * Write log to console with magenta color.
+ * @param {string} content the log content
+ */
 module.exports.log = content => console.log(chalk.magentaBright(content))
 
+/**
+ * Download a repository with a elegant terminal spinner.
+ * @param {string} repo the repository path in gitHub
+ * @param {string} desc the destination of downloaded repository
+ */
 module.exports.clone = async (repo, desc) => {
     const process = ora(`Download ${repo}`);
     process.start();
@@ -17,11 +27,33 @@ module.exports.clone = async (repo, desc) => {
     process.succeed()
 }
 
-module.exports.spawn_pipe = async (...args) => {
+/**
+ * Execute Shell command and pipe output to console
+ * @param {...} args the command and its configuration, e.g. work directory
+ */
+module.exports.spawn_console = async (...args) => {
     return new Promise(resolve => {
         const proc = spawn(...args)
         proc.stdout.pipe(process.stdout)
         proc.stderr.pipe(process.stderr)
+        proc.on('close', () => {
+            resolve()
+        })
+    })
+}
+
+
+/**
+ * Execute Shell command, separate subprocess if necessary
+ * @param {boolean} unref the flag for separating the subprocess from the main process
+ * @param {...} args the command and its configuration, e.g. environment variables
+ */
+module.exports.spawn_log = async (unref, ...args) => {
+    return new Promise(resolve => {
+        const proc = spawn(...args)
+        if (unref){
+            proc.unref()
+        }
         proc.on('close', () => {
             resolve()
         })
